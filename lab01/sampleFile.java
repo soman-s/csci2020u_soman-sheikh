46f4d3eacc876d0d0aaa3f3cd8b0b83df466f49c
Soman Sheikh <somansheikh786@gmail.com>
2021-01-20 00:29:06 -0500

    testing git commands for branches

---
+++ lab01/sampleFile.java
@@ -0,0 +1,5 @@
+public class sampleFile{
+    public static void main(String[] args){
+        System.out.println("This is a sample file for the beta branch");
+    }
+}
---
+++ lab01/testFile.java
@@ -0,0 +1,7 @@
+public class testFile{
+    public static void main(String[] args){
+        System.out.println("This is a test file for experimenting with git commands");
+
+        System.out.println("Making changes in the master branch");
+    }
+}
